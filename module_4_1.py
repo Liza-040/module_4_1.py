14d6579351fb1c9348aae7ccdc0ce803b5443319
Liza-040 <04042006liza@gmail.com>
2024-10-20 22:58:09 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+import fake_math as f_m
+import true_math as tm
+
+result1 = f_m.divide_f(69, 3)
+result2 = f_m.divide_f(3, 0)
+result3 = tm.divide_t(49, 7)
+result4 = tm.divide_t(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ true_math.py
@@ -0,0 +1,5 @@
+from math import inf
+def divide_t(first, second):
+    if second == 0:
+        return inf
+    return first / second
